a64e6fe616cf46199af13b75fdace358e11d644f
lauichan <te06230623@gmail.com>
2024-02-23 09:52:46 +0900

    수정

--- tic-tac-toe/vite/vite-tic-tac-toe/src/App.jsx
+++ tic-tac-toe/vite/vite-tic-tac-toe/src/App.jsx
@@ -1,51 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
-import { gameOver, setBoard } from "./store/module/game";
-import React from "react";
-
-const COMPLETE_LIST = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+import { resetBoard, updateBoard } from "./store/module/game";
 
 function App() {
   const dispatch = useDispatch();
-  const { current, board, xlist, olist, finish } = useSelector((state) => state.game);
+  const { board, winner } = useSelector((state) => state.game);
 
-  const checkComplete = (list) => {
-    for (const line of COMPLETE_LIST) {
-      if (line.every((value) => list.includes(value))) {
-        dispatch(gameOver());
-        return true;
-      }
-    }
-    return false;
+  const handleClickBoard = (e) => {
+    dispatch(updateBoard(+e.target.name));
   };
 
-  const handleClick = (e) => {
-    if (finish) return;
-    dispatch(setBoard({ idx: +e.target.name, value: current ? "O" : "X" }));
-    e.target.disabled = true;
+  const handleResetBtn = () => {
+    dispatch(resetBoard());
   };
 
   return (
     <>
-      {checkComplete(xlist) ? <p>X가 승리했습니다.</p> : null}
-      {checkComplete(olist) ? <p>O가 승리했습니다.</p> : null}
+      {winner && <p>{winner}가 승리했습니다.</p>}
       <section className="board">
         {board.map((button, i) => {
           return (
-            <button onClick={handleClick} name={i}>
+            <button key={i} onClick={handleClickBoard} name={i} disabled={button !== null}>
               {button}
             </button>
           );
         })}
       </section>
+      <button className="resetBtn" onClick={handleResetBtn}>
+        reset
+      </button>
     </>
   );
 }
--- tic-tac-toe/vite/vite-tic-tac-toe/src/store/module/game.js
+++ tic-tac-toe/vite/vite-tic-tac-toe/src/store/module/game.js
@@ -1,35 +1,62 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const COMPLETE_LIST = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const initialState = {
   current: true,
-  board: [null, null, null, null, null, null, null, null, null],
+  board: Array(9).fill(null),
   olist: [],
   xlist: [],
-  finish: false,
+  winner: null,
 };
 
 const gameSlice = createSlice({
   name: "game",
   initialState,
   reducers: {
-    setBoard: (state, action) => {
-      if (state.finish) return;
+    updateBoard: (state, action) => {
+      if (state.winner) return;
 
-      state.board[action.payload.idx] = state.current ? "X" : "O";
+      const currentPlayer = state.current ? "X" : "O";
+      state.board[action.payload] = currentPlayer;
 
       if (state.current) {
-        state.olist.push(action.payload);
-      } else {
         state.xlist.push(action.payload);
+        if (checkComplete(state.xlist)) state.winner = currentPlayer;
+      } else {
+        state.olist.push(action.payload);
+        if (checkComplete(state.olist)) state.winner = currentPlayer;
       }
 
       state.current = !state.current;
     },
-    gameOver: (state, action) => {
-      state.finish = true;
+    resetBoard: (state) => {
+      state.current = true;
+      state.board = Array(9).fill(null);
+      state.olist = [];
+      state.xlist = [];
+      state.winner = null;
     },
   },
 });
 
-export const { setBoard, gameOver } = gameSlice.actions;
+export const { updateBoard, resetBoard } = gameSlice.actions;
 export default gameSlice.reducer;
+
+const checkComplete = (list) => {
+  for (const line of COMPLETE_LIST) {
+    if (line.every((value) => list.includes(value))) {
+      return true;
+    }
+  }
+  return false;
+};
